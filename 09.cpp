1939f9a7cce19856404f5ace54546609dcb5dac8
Sumit Kumar <ksumit1454@gmail.com>
2026-09-17 22:53:52 +0530

    09

---
+++ 09.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x) {
+    if (x < 0) {
+        return false;
+    }
+
+    int original = x;
+    int reverse = 0;
+
+    while (x > 0) {
+        int digit = x % 10;
+        reverse = reverse * 10 + digit;
+        x = x / 10;
+    }
+
+    return original == reverse;
+}
+
+int main() {
+    int x;
+    cin >> x;
+
+    cout << isPalindrome(x);
+
+    return 0;
+}
